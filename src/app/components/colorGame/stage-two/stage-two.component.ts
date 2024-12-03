afb6c259222fe3516b78634a3194e7461d38d836
JuanAlberto1278 <juanalberto22112@gmail.com>
2024-12-03 09:38:30 -0600

    2
    2

--- src/app/components/colorGame/stage-two/stage-two.component.ts
+++ src/app/components/colorGame/stage-two/stage-two.component.ts
@@ -1,68 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { ScoreService } from '../../../services/score.service';
+import { ModalService } from '../../../services/modal.service';
+import { GamesKnoledgeBaseComponent } from '../../game/games-knoledge-base/games-knoledge-base.component';
+import { GamesSaveScoreComponent } from '../../game/games-save-score/games-save-score.component';
 
 @Component({
   selector: 'app-stage-two',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    GamesKnoledgeBaseComponent,
+    GamesSaveScoreComponent],
   templateUrl: './stage-two.component.html',
   styleUrls: ['./stage-two.component.scss']
 })
 export class StageTwoComponent {
   currentQuestionIndex = 0;
-  score = 0;
+  correctAnswers = 0;
+  wrongAnswers = 0;
+  selectedGame: number = 0;
   selectedOption: string | null = null;
   isAnswerCorrect: boolean | null = null;
   showResult = false;
   isInteractionDisabled = false; // Nueva bandera para deshabilitar botones
+  
+  public scoreService: ScoreService = inject(ScoreService);
+  public modalService: ModalService = inject(ModalService);
 
+  @ViewChild('scoreModal') public scoreModal: any;
+  constructor(){
+    this.selectedGame = 5;
+  }
   @Output() complete = new EventEmitter<void>();
   unlockAllQuestions(): void {
-    // Lógica para desbloquear todos los colores
     this.complete.emit(); // Notifica al componente padre
   }
 
   questions = [
     {
       question: '¿Cómo se obtenía históricamente el pigmento rojo?',
-      options: [ 'De minerales de arsénico', 'De la cochinilla', 'De hojas secas'],
+      options: ['De la cochinilla', 'De minerales de arsénico', 'De hojas secas'],
       correctAnswer: 'De la cochinilla'
     },
     {
       question: '¿Cuál es la fuente principal para obtener el pigmento azul ultramar?',
       options: ['Lapislázuli', 'Flores de jacaranda', 'Cielo al amanecer'],
       correctAnswer: 'Lapislázuli'
-    },
-    {
-      question: '¿Qué fuente natural se usaba para obtener amarillo?',
-      options: ['Oro en polvo','Cúrcuma', 'Hojas de arce'],
-      correctAnswer: 'Cúrcuma'
-    },
-    {
-      question: '¿Qué se utilizaba históricamente para obtener pigmentos naranjas?',
-      options: ['Minerales como el realgar', 'Hojas de zanahoria', 'Flores secas de caléndula'],
-      correctAnswer: 'Minerales como el realgar'
-    },
-    {
-      question: '¿Qué se usaba para obtener tonos verdes?',
-      options: ['Hojas de albahaca', 'Pasto fermentado','Malaquita triturada'],
-      correctAnswer: 'Malaquita triturada'
-    },
-    {
-      question: '¿Qué combinación de minerales daba lugar al púrpura?',
-      options: ['Oxidación del cobre','Murex', 'Polvo de amatista'],
-      correctAnswer: 'Murex'
-    },
-    {
-      question: '¿Qué combinación de pigmentos se usaba para obtener azul verdoso?',
-      options: ['Azul ultramar y malaquita', 'Cúrcuma y lapislázuli', 'Realgar y murex'],
-      correctAnswer: 'Azul ultramar y malaquita'
-    },
-    {
-      question: '¿Cuál era el origen del pigmento ámbar?',
-      options: ['Hojas de arce y polvo de amatista','Cúrcuma y minerales', 'Flores secas y realgar'],
-      correctAnswer: 'Cúrcuma y minerales'
     }
   ];
 
@@ -75,22 +59,31 @@ export class StageTwoComponent {
     this.showResult = true;
     this.isInteractionDisabled = true; // Bloquear interacción
 
+    // Incrementar contadores de respuestas correctas o incorrectas
+    if (this.isAnswerCorrect) {
+      this.correctAnswers++;
+    } else {
+      this.wrongAnswers++;
+    }
+
     // Esperar 2 segundos antes de avanzar
     setTimeout(() => {
       this.showResult = false;
       this.isInteractionDisabled = false; // Desbloquear interacción
       this.selectedOption = null;
       this.currentQuestionIndex++;
-    }, 2000);
 
-    if (this.isAnswerCorrect) {
-      this.score++;
-    }
+      // Verificar si el cuestionario ha terminado
+      if (this.isQuizCompleted()) {
+        this.saveScore(this.correctAnswers, this.wrongAnswers);
+      }
+    }, 2000);
   }
 
   resetQuiz() {
     this.currentQuestionIndex = 0;
-    this.score = 0;
+    this.correctAnswers = 0;
+    this.wrongAnswers = 0;
     this.selectedOption = null;
     this.isAnswerCorrect = null;
     this.showResult = false;
@@ -99,5 +92,17 @@ export class StageTwoComponent {
 
   isQuizCompleted(): boolean {
     return this.currentQuestionIndex >= this.questions.length;
+  }
+
+  saveScore(correct: number, wrong: number) {
+    // Guardar los resultados del puntaje
+    this.correctAnswers = correct;
+    this.wrongAnswers = wrong;
+    this.selectedGame = 5; // Puedes cambiar el valor según la lógica de tu juego
+    this.modalService.displayModal('md', this.scoreModal);
+  }
+
+  closeModal(){
+    this.modalService.closeAll();
   }
 }
--- src/app/components/game/games-save-score/games-save-score.component.ts
+++ src/app/components/game/games-save-score/games-save-score.component.ts
@@ -22,24 +22,25 @@ export class GamesSaveScoreComponent implements AfterViewInit {
   public modalService: ModalService = inject(ModalService);
   scoreResponse: string = '';
   @ViewChild('scoreModal') public scoreModal: any;
-  @ViewChild('keyboardScore') keyboarSection!: ElementRef <HTMLDivElement>;
-  @ViewChild('colorScore') colorSection!:  ElementRef <HTMLDivElement>;
+  @ViewChild('keyboardScore') keyboarSection!: ElementRef<HTMLDivElement>;
+  @ViewChild('colorScore') colorSection!: ElementRef<HTMLDivElement>;
+  @ViewChild('standardContinueButton') standardContinueButton!: ElementRef<HTMLDivElement>;
   @Output() calculationInit = new EventEmitter<number>();
   @Input() selectedGameId!: number;
   @Input() wrongAnswers!: number;
   @Input() correctAnswers!: number;
-  closeModal(){
+  closeModal() {
     this.modalService.closeAll();
   }
 
-  ngAfterViewInit(){
-    if (this.selectedGameId == 1){
+  ngAfterViewInit() {
+    if (this.selectedGameId == 1) {
       console.log("enchó");
       this.keyboarSection.nativeElement.classList.remove('display-none');
-      let score: IScore ={
+      let score: IScore = {
         rightAnswers: this.correctAnswers,
         wrongAnswers: this.wrongAnswers,
-        game: 	{
+        game: {
           "id": 1,
           "name": "Key",
           "description": "GeneratedGame",
@@ -47,14 +48,54 @@ export class GamesSaveScoreComponent implements AfterViewInit {
           "updatedAt": "2024-11-16T00:11:01.000+00:00"
         },
       }
-  this.scoreService.save(score).subscribe({
-    next: (response) => {
-        this.starsEarned = response.stars;
-        this.correctAnswers = response.rightAnswers;
-    },
-    error: (err) => {
-        console.error('Error saving score:', err);
+      this.scoreService.save(score).subscribe({
+        next: (response) => {
+          this.starsEarned = response.stars;
+          this.correctAnswers = response.rightAnswers;
+        },
+        error: (err) => {
+          console.error('Error saving score:', err);
+        }
+      });
+    } else if (this.selectedGameId == 5) {
+      console.log("prapra");
+      this.colorSection.nativeElement.classList.remove('display-none');
+      this.standardContinueButton.nativeElement.classList.add('display-none');
+      let score: IScore = {
+        rightAnswers: this.correctAnswers,
+        wrongAnswers: this.wrongAnswers,
+        game: {
+          "id": 5,
+          "name": "Color",
+          "description": "GeneratedGame",
+          "createdAt": "2024-11-16T00:10:20.000+00:00",
+          "updatedAt": "2024-11-16T00:11:01.000+00:00"
+        },
+      }
+      this.scoreService.save(score).subscribe({
+        next: (response) => {
+          this.starsEarned = response.stars;
+          this.correctAnswers = response.rightAnswers;
+        },
+        error: (err) => {
+          console.error('Error saving score:', err);
+        }
+      });
     }
-  });
-}}
+  }
+
+  @Output() resetQuizEvent = new EventEmitter<void>();  // Evento para reiniciar el quiz
+  @Output() unlockAllQuestionsEvent = new EventEmitter<void>();  // Evento para continuar
+
+  constructor() {}
+
+  // Método para emitir el evento de reinicio
+  onResetQuiz() {
+    this.resetQuizEvent.emit();  // Emite el evento
+  }
+
+  // Método para emitir el evento de continuar
+  onUnlockAllQuestions() {
+    this.unlockAllQuestionsEvent.emit();  // Emite el evento
+  }
 }
